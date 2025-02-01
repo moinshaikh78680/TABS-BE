89ca208e5be925d34950b2d40888409b327f313d
monshaikh78680 <moinshaikh78680@gmail.com>
2025-02-01 16:37:26 +0530

    checkDB

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("MONGO DB",process.env.MONGO_URI)
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://moinshaikh78680:Master0947@tabs.ettie.mongodb.net/tabs", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
